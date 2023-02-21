b342eb09f136aab88d887a764ffc93d16b1943f2
João Gabriel R Cardoso <joaogabriel@cnnct.black>
2023-02-21 07:42:58 -0300

    Class 05 - Tests

---
+++ 05-tests/src/tests.js
@@ -0,0 +1,56 @@
+const assert = require('assert');
+const nock = require('nock');
+const { getPeople } = require('./service');
+
+describe('Star Wars Tests', function () {
+  this.beforeAll(() => {
+    const mockGetPeople = {
+      count: 1,
+      next: null,
+      previous: null,
+      results: [
+        {
+          name: 'R2-D2',
+          height: '96',
+          mass: '32',
+          hair_color: 'n/a',
+          skin_color: 'white, blue',
+          eye_color: 'red',
+          birth_year: '33BBY',
+          gender: 'n/a',
+          homeworld: 'https://swapi.dev/api/planets/8/',
+          films: [
+            'https://swapi.dev/api/films/1/',
+            'https://swapi.dev/api/films/2/',
+            'https://swapi.dev/api/films/3/',
+            'https://swapi.dev/api/films/4/',
+            'https://swapi.dev/api/films/5/',
+            'https://swapi.dev/api/films/6/'
+          ],
+          species: ['https://swapi.dev/api/species/2/'],
+          vehicles: [],
+          starships: [],
+          created: '2014-12-10T15:11:50.376000Z',
+          edited: '2014-12-20T21:17:50.311000Z',
+          url: 'https://swapi.dev/api/people/3/'
+        }
+      ]
+    };
+
+    nock('https://swapi.dev/api/people')
+      .get('/?search=r2-d2&format=json')
+      .reply(200, mockGetPeople);
+  });
+
+  it('should search r2d2', async () => {
+    // given
+    const name = 'r2-d2';
+
+    // when
+    const people = await getPeople(name);
+    const result = people.results;
+
+    // then
+    assert.deepEqual(result[0].name, 'R2-D2');
+  });
+});
